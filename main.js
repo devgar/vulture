a9cb3c281fd7f489be334dd8084d339494e18993
Edgar Albalate <dev.gardo@gmail.com>
2017-05-15 07:48:26 +0200

    Added some modifications in config scheme

--- main.js
+++ main.js
@@ -13,26 +13,25 @@ function chckentities ( tweet, user ){
     if(us[i].screen_name.toLowerCase() == user) return true;
   }
   return false;
-} 
+}
 
 const Twit = require('twit');
 
-const T = new Twit(require('./config.json'));
+const T = new Twit(require('./config.json').login );
 
-var stream = T.stream('statuses/filter', { follow: ['85646556', '160571292', '90886814'] });
+var stream = T.stream('statuses/filter', { follow: require('./config.json').follow });
 
 stream.on('tweet', tweet => {
   if(tweet.in_reply_to_status_id) return;
   if(tweet.retweeted_status) return;
-  if(!chckentities(tweet, 'startrespodcast')) 
-    return console.log('Tweet so close to retweet:\n', tweet);
-  
+  if(!chckentities(tweet, require('./config.json').key)) return;
+
   setTimeout( ()=> {
-    T.post('statuses/retweet/:id', { id: tweet.id_str }, 
+    T.post('statuses/retweet/:id', { id: tweet.id_str },
     (err, data, response) => {
       if(err) return console.log('ERR:', err);
       console.log('Retweeted', tweet.id, '=>', data.id, '\n');
     });
   }, 1000 + random(10000));
-    
+
 });
